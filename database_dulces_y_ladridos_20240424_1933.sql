b43ea952c02c3465d6ce24e9cad546abbab29a84
Carlos Chongo <cacc.chongo@gmail.com>
2024-04-24 19:56:00 -0600

    Se implementan observaciones realizadas por Tan

---
+++ database_dulces_y_ladridos_20240424_1933.sql
@@ -0,0 +1,309 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dulces_y_ladridos
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories_posts`
+--
+
+DROP TABLE IF EXISTS `categories_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories_posts` (
+  `id_category_post` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_category_post`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories_posts`
+--
+
+LOCK TABLES `categories_posts` WRITE;
+/*!40000 ALTER TABLE `categories_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories_products`
+--
+
+DROP TABLE IF EXISTS `categories_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories_products` (
+  `id_category_product` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_category_product`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories_products`
+--
+
+LOCK TABLES `categories_products` WRITE;
+/*!40000 ALTER TABLE `categories_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id_order` bigint NOT NULL AUTO_INCREMENT,
+  `date_order` timestamp NOT NULL,
+  `id_user` bigint NOT NULL,
+  `id_review` bigint NOT NULL,
+  PRIMARY KEY (`id_order`),
+  KEY `fk_orders_users1_idx` (`id_user`),
+  KEY `fk_orders_reviews1_idx` (`id_review`),
+  CONSTRAINT `fk_orders_reviews1` FOREIGN KEY (`id_review`) REFERENCES `reviews` (`id_review`),
+  CONSTRAINT `fk_orders_users1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders_has_products`
+--
+
+DROP TABLE IF EXISTS `orders_has_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders_has_products` (
+  `id_order_has_product` bigint NOT NULL AUTO_INCREMENT,
+  `id_order` bigint NOT NULL,
+  `id_product` bigint NOT NULL,
+  `amount` decimal(9,2) NOT NULL,
+  PRIMARY KEY (`id_order_has_product`,`id_order`,`id_product`),
+  KEY `fk_orders_has_products_products1_idx` (`id_product`),
+  KEY `fk_orders_has_products_orders1_idx` (`id_order`),
+  CONSTRAINT `fk_orders_has_products_orders1` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`),
+  CONSTRAINT `fk_orders_has_products_products1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_products`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders_has_products`
+--
+
+LOCK TABLES `orders_has_products` WRITE;
+/*!40000 ALTER TABLE `orders_has_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders_has_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id_post` bigint NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date` date NOT NULL,
+  `id_user` bigint NOT NULL,
+  `id_category_post` bigint NOT NULL,
+  PRIMARY KEY (`id_post`,`id_category_post`),
+  UNIQUE KEY `title_UNIQUE` (`title`),
+  KEY `fk_posts_users1_idx` (`id_user`),
+  KEY `fk_posts_categories_posts1_idx` (`id_category_post`),
+  CONSTRAINT `fk_posts_categories_posts1` FOREIGN KEY (`id_category_post`) REFERENCES `categories_posts` (`id_category_post`),
+  CONSTRAINT `fk_posts_users1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `privileges`
+--
+
+DROP TABLE IF EXISTS `privileges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `privileges` (
+  `id_privilege` bigint NOT NULL AUTO_INCREMENT,
+  `privilege` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_privilege`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `privileges`
+--
+
+LOCK TABLES `privileges` WRITE;
+/*!40000 ALTER TABLE `privileges` DISABLE KEYS */;
+/*!40000 ALTER TABLE `privileges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id_products` bigint NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `dogo_name` varchar(45) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `price` decimal(9,2) NOT NULL,
+  `quantity` int NOT NULL,
+  `image_URL` varchar(200) NOT NULL,
+  `id_category_product` bigint NOT NULL,
+  PRIMARY KEY (`id_products`,`id_category_product`),
+  UNIQUE KEY `dogo_name_UNIQUE` (`dogo_name`),
+  KEY `fk_products_categories_products1_idx` (`id_category_product`),
+  CONSTRAINT `fk_products_categories_products1` FOREIGN KEY (`id_category_product`) REFERENCES `categories_products` (`id_category_product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `id_review` bigint NOT NULL AUTO_INCREMENT,
+  `content` varchar(150) NOT NULL,
+  `rating` int NOT NULL,
+  `date` date NOT NULL,
+  `validation` tinyint NOT NULL,
+  `id_user` bigint NOT NULL,
+  PRIMARY KEY (`id_review`),
+  KEY `fk_reviews_users1_idx` (`id_user`),
+  CONSTRAINT `fk_reviews_users1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_user` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `telephone_number` varchar(20) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_has_privileges`
+--
+
+DROP TABLE IF EXISTS `users_has_privileges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_has_privileges` (
+  `id_user_has_privilege` bigint NOT NULL AUTO_INCREMENT,
+  `id_user` bigint NOT NULL,
+  `id_privilege` bigint NOT NULL,
+  PRIMARY KEY (`id_user_has_privilege`,`id_user`,`id_privilege`),
+  KEY `fk_users_has_privileges_privileges1_idx` (`id_privilege`),
+  KEY `fk_users_has_privileges_users_idx` (`id_user`),
+  CONSTRAINT `fk_users_has_privileges_privileges1` FOREIGN KEY (`id_privilege`) REFERENCES `privileges` (`id_privilege`),
+  CONSTRAINT `fk_users_has_privileges_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_has_privileges`
+--
+
+LOCK TABLES `users_has_privileges` WRITE;
+/*!40000 ALTER TABLE `users_has_privileges` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_has_privileges` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-24 19:34:04
