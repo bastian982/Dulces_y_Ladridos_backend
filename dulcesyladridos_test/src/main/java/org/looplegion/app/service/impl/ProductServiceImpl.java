128763c30410386d25d931d399418fd1aba3de44
Alera9 <mcarrazanag@gmail.com>
2024-04-29 17:06:48 -0600

    Service producto

---
+++ dulcesyladridos_test/src/main/java/org/looplegion/app/service/impl/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package org.looplegion.app.service.impl;
+
+import java.util.List;
+
+import org.looplegion.app.entity.Product;
+import org.looplegion.app.entity.ProductCategory;
+import org.looplegion.app.service.ProductService;
+
+public class ProductServiceImpl implements ProductService{
+
+	@Override
+	public Product getProductById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByDogoName(String dogoName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getAllByCategory(ProductCategory productCategory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product updateProduct(Product product, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteProduct(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
