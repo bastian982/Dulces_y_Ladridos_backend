f5e3be29f68b4d5265d78b350038f3752078ce3c
Alera9 <mcarrazanag@gmail.com>
2024-04-30 00:17:08 -0600

    Create: ProductRepository, ProductService, ProductServiceImpl, ProductController

--- dulcesyladridos_test/src/main/java/org/looplegion/app/service/impl/ProductServiceImpl.java
+++ dulcesyladridos_test/src/main/java/org/looplegion/app/service/impl/ProductServiceImpl.java
@@ -1,17 +1,47 @@
 package org.looplegion.app.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.looplegion.app.entity.Product;
 import org.looplegion.app.entity.ProductCategory;
+import org.looplegion.app.repository.ProductRepository;
 import org.looplegion.app.service.ProductService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductServiceImpl implements ProductService{
+	
+	ProductRepository productRepository;
+	
+	//Constructor
+	public ProductServiceImpl(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
 
 	@Override
 	public Product getProductById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Product> productOptional = productRepository.findById(id);
+		Product existingProduct;
+		
+		if(productOptional.isPresent()) {
+			existingProduct = productOptional.get();
+			return existingProduct;
+		}else {
+			throw new IllegalStateException("User does not exist with id " + id);
+		}
+	}
+	
+	@Override
+	public Product createProduct(Product product) {
+		
+		product.setId(null);
+		
+		if(productRepository.existsByDogoName(product.getDogoName())) {
+			throw new IllegalStateException("Product already exists with that dogo name" +product.getDogoName());
+		}
+		
+		return productRepository.save(product);
 	}
 
 	@Override
@@ -35,13 +65,29 @@ public class ProductServiceImpl implements ProductService{
 	@Override
 	public Product updateProduct(Product product, Long id) {
 		// TODO Auto-generated method stub
-		return null;
+		Product existingProduct = getProductById(id);
+		existingProduct.setName(product.getName());
+		existingProduct.setDogoName(product.getDogoName());
+		existingProduct.setDescription(product.getDescription());
+		existingProduct.setPrice(product.getPrice());
+		existingProduct.setQuantity(product.getQuantity());
+		existingProduct.setImageUrl(product.getImageUrl());
+		existingProduct.setProductcategory(product.getProductcategory());
+		return productRepository.save(existingProduct);
 	}
 
 	@Override
 	public void deleteProduct(Long id) {
-		// TODO Auto-generated method stub
+		Product existingProduct = getProductById(id);
+		productRepository.delete(existingProduct);
 		
 	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
