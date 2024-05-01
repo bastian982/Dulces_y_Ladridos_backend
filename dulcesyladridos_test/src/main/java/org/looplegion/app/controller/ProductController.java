9c8bcd742f7418cbc5f5818dbd62a30c36c27ab3
Carlos Chongo <cacc.chongo@gmail.com>
2024-04-30 20:39:31 -0600

    se agregan archivos product, por rectificar

---
+++ dulcesyladridos_test/src/main/java/org/looplegion/app/controller/ProductController.java
@@ -0,0 +1,61 @@
+package org.looplegion.app.controller;
+
+import java.util.List;
+
+import org.looplegion.app.entity.Product;
+import org.looplegion.app.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")//  indica que se permite el acceso a los recursos desde cualquier origen, lo que significa que la API puede ser accedida desde cualquier dominio sin restricciones de origen cruzado.
+@RestController
+@RequestMapping("api/v1/products")
+
+public class ProductController {
+	ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping
+	ResponseEntity<List<Product>> getAllProducts(
+			@RequestParam(name="active",
+										required =false,
+										defaultValue = "true") boolean active
+					){
+		return new ResponseEntity<List<Product>>( productService.getAllProducts(), HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}") // localhost:8080/api/v1/products/{id}
+	ResponseEntity<Product> getProductById(@PathVariable("id") Long id ){
+		return new ResponseEntity<Product>(productService.getProductById(id) ,HttpStatus.OK );
+	}
+	
+	@PostMapping
+	ResponseEntity<Product> createProduct(@RequestBody Product product ){
+		Product createdProduct = productService.createProduct(product);
+		
+		return new ResponseEntity<Product>( createdProduct, HttpStatus.CREATED );		
+	}
+	
+	@DeleteMapping("{id}")
+	ResponseEntity<String> deleteProduct(@PathVariable("id") Long id ){
+		productService.deleteProduct(id);
+		return new ResponseEntity<String>("Product id " + id + " successfully deleted", HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	ResponseEntity<Product> updateProduct(
+			@RequestBody Product product, 
+			@PathVariable("id") Long id  
+			){
+		Product updatedProduct = productService.updateProduct(product, id);
+		
+		return new ResponseEntity<Product>( updatedProduct, HttpStatus.OK );		
+	}
+	
+	
+	
+}
